eeb76df008883afd054438a04328048e893752a5
zuwannn <suwanneethongtha@gmail.com>
2021-09-28 02:42:56 +0700

    Add Home

--- react-multi-page/src/App.js
+++ react-multi-page/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import { Navigation, Footer } from "./components";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigation, Footer, Home } from "./components";
 function App() {
   return (
     <div className="App">
       <Router>
         <Navigation />
         <Switch>
+          <Route path="/" exact component={() => <Home />} />
           
         </Switch>
-        <Footer />  
+        <Footer />
       </Router>
     </div>
   );
