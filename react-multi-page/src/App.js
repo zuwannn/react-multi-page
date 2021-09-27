e232bf6f526aee2ba0af606d1f2b0b56fe7f52a3
zuwannn <suwanneethongtha@gmail.com>
2021-09-28 02:44:16 +0700

    Add Contact

--- react-multi-page/src/App.js
+++ react-multi-page/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navigation, Footer, Home, About } from "./components";
+import { Navigation, Footer, Home, About, Contact } from "./components";
 function App() {
   return (
     <div className="App">
@@ -9,6 +9,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={() => <Home />} />
           <Route path="/about" exact component={() => <About />} />
+          <Route path="/contact" exact component={() => <Contact />} />
         </Switch>
         <Footer />
       </Router>
