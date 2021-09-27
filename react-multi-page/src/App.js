4d472aee066c59a428a2b2db3e08d4a1372fbd96
zuwannn <suwanneethongtha@gmail.com>
2021-09-28 02:43:37 +0700

    Add About

--- react-multi-page/src/App.js
+++ react-multi-page/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navigation, Footer, Home } from "./components";
+import { Navigation, Footer, Home, About } from "./components";
 function App() {
   return (
     <div className="App">
@@ -8,7 +8,7 @@ function App() {
         <Navigation />
         <Switch>
           <Route path="/" exact component={() => <Home />} />
-          
+          <Route path="/about" exact component={() => <About />} />
         </Switch>
         <Footer />
       </Router>
